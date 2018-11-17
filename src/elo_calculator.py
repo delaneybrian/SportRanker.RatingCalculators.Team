aea8b0f6e782ae177b93253d497746c463db2cc8
= <=>
2018-11-17 14:53:16 +0000

    fixed rankings and added home adv

--- src/elo_calculator.py
+++ src/elo_calculator.py
@@ -18,26 +18,41 @@ class EloCalculator:
 
         home_team_adv = self.home_adv_calculator.calculate_home_advantage(sport)
 
-        #remove ht adv
-        diff_in_ratings = math.fabs((home_orignal_rating) - away_orignal_rating)
+        adjusted_home_rating = home_team_adv + home_orignal_rating
 
-        expected_result = self.calculate_win_expectency(diff_in_ratings)
+        home_expected_result, away_expected_result = self.calculate_expected_results(adjusted_home_rating, away_orignal_rating)
 
         if(home_score > away_score):
-            home_new_rating = self.calculate_new_rating(self.win_outcome, expected_result, k_value, home_orignal_rating)
-            away_new_rating = self.calculate_new_rating(self.lose_outcome, expected_result, k_value, away_orignal_rating)
+            home_new_rating = self.calculate_new_rating(self.win_outcome, home_expected_result, k_value, home_orignal_rating)
+            away_new_rating = self.calculate_new_rating(self.lose_outcome, away_expected_result, k_value, away_orignal_rating)
             return home_new_rating, away_new_rating
         elif(home_score < away_score):
-            home_new_rating = self.calculate_new_rating(self.lose_outcome, expected_result, k_value, home_orignal_rating)
-            away_new_rating = self.calculate_new_rating(self.win_outcome, expected_result, k_value, away_orignal_rating)
+            home_new_rating = self.calculate_new_rating(self.lose_outcome, home_expected_result, k_value, home_orignal_rating)
+            away_new_rating = self.calculate_new_rating(self.win_outcome, away_expected_result, k_value, away_orignal_rating)
             return home_new_rating, away_new_rating
         else:
-            home_new_rating = self.calculate_new_rating(self.draw_outcome, expected_result, k_value, home_orignal_rating)
-            away_new_rating = self.calculate_new_rating(self.draw_outcome, expected_result, k_value, away_orignal_rating)
+            home_new_rating = self.calculate_new_rating(self.draw_outcome, home_expected_result, k_value, home_orignal_rating)
+            away_new_rating = self.calculate_new_rating(self.draw_outcome, away_expected_result, k_value, away_orignal_rating)
             return home_new_rating, away_new_rating
 
+    def calculate_expected_results(self, home_orignal_rating, away_orignal_rating):
+
+        diff_in_ratings = math.fabs((home_orignal_rating) - away_orignal_rating)
+
+        if(home_orignal_rating > away_orignal_rating):
+            home_expected_result = self.calculate_win_expectency(diff_in_ratings)
+            away_expected_result = 1 - home_expected_result
+        elif(home_orignal_rating < away_orignal_rating):
+            away_expected_result = self.calculate_win_expectency(diff_in_ratings)
+            home_expected_result = 1 - away_expected_result
+        else:
+            home_expected_result = 0.5
+            away_expected_result = 0.5
+
+        return home_expected_result, away_expected_result
+
     def calculate_new_rating(self, result, expected_result, k_value, orignal_rating):
-        return orignal_rating + (k_value * (expected_result - result))
+        return orignal_rating + (k_value * (result - expected_result))
 
     def calculate_win_expectency(self, difference_in_ratings):
 
--- src/main.py
+++ src/main.py
@@ -1,17 +1,16 @@
 from elo_calculator import EloCalculator
 from consumer import Consumer
 
-consumer = Consumer()
+#consumer = Consumer()
 
-consumer.start_consumer()
+#consumer.start_consumer()
 
-"""
+
 elo_calculator = EloCalculator()
 
-home_rating, away_rating = elo_calculator.calculate_new_ratings("nfl", 1500, 1800, 30, 30)
+home_rating, away_rating = elo_calculator.calculate_new_ratings("nhl", 1500, 1500, 00, 00)
 
 print("Home Rating:")
 print(home_rating)
 print("Away Rating:")
 print(away_rating)
-"""
