38251cefb15816b553f0f8ca68ed392946cab2b9
= <=>
2018-12-21 17:06:13 +0000

    added time delay

--- src/main.py
+++ src/main.py
@@ -1,10 +1,14 @@
 from consumer import Consumer
 from logger import Logger
+import time
 
 consumer = Consumer()
 logger = Logger()
 
 while(True):
+    try:
         logger.info_log("Starting Message Consumer..")
         consumer.start_consumer()
+    except :
         logger.warning_log("Error Running Consumer. Restarting...")
+        time.sleep(10000)
