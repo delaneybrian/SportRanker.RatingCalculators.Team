532fe2ba2b007e0b595e7bbc0012ca494e2f492e
= <=>
2018-12-23 15:40:44 +0000

    updated logger

--- src/logger.py
+++ src/logger.py
@@ -3,7 +3,7 @@ import logging
 class Logger:
 
     def __init__(self):
-        logging.basicConfig(filename=r"C:\Code\SportRanker\Logs\logs.txt", level=logging.INFO)
+        logging.basicConfig(filename=r"logs/location.txt", level=logging.INFO)
 
     def debug_log(self, message):
         logging.debug(message)
