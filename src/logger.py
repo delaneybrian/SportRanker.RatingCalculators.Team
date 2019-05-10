e884800ed237858838063abaec311df727831729
= <=>
2019-05-10 23:39:02 +0100

    updated logger and requirements

--- src/logger.py
+++ src/logger.py
@@ -1,9 +1,12 @@
 import logging
+import os
 
 class Logger:
 
     def __init__(self):
-        logging.basicConfig(filename=r"logs/location.txt", level=logging.INFO)
+        if not os.path.exists('teams.txt'):
+            with open('teams.txt', 'w'): pass
+        logging.basicConfig(filename=r"teams.txt", level=logging.INFO)
 
     def debug_log(self, message):
         logging.debug(message)
